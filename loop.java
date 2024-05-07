338174e6ef593b5c6c66a382b8c8bf141dc433a8
prathyusha <154443488+hsdahf@users.noreply.github.com>
2024-05-07 07:49:33 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,17 @@
+public class Example3 {
+
+	public static void main(String[] args) {
+		int a=15,b=15;
+		while(a<10)
+		{
+			a++;
+			System.out.print(a+"  ");
+		}
+		System.out.println("\n---------------");
+		do 
+		{
+			b++;
+			System.out.print(b+"  ");
+		}while(b<10);
+	}
+}
