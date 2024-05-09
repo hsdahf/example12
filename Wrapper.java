cf44f03be69a3169b27f5e94bce1036f149945eb
prathyusha <154443488+hsdahf@users.noreply.github.com>
2024-05-09 16:24:52 +0530

    Add files via upload

---
+++ Wrapper.java
@@ -0,0 +1,18 @@
+public class Wrapper {
+
+	public static void main(String[] args) 
+	{
+		                 
+		byte a=1;        
+		int b=6;          
+		float c=8.9f;   
+		double d=67.54;
+		char e='g';    
+		boolean f=false;
+		System.out.println(a1+" "+b1+" "+c1+" "+d1+" "+e1+" "+f1);
+		Integer i1=new Integer(50);
+		int x=i1;
+		System.out.println(x);
+	}
+	
+	}
